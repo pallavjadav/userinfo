5d3c96641e0852cfb72a001716adaa7d2c18846f
Pallavkumar Jadav <pallavjadav@gmail.com>
2023-05-18 07:15:25 +0000

    added ui5

---
+++ app/resources/webapp/controller/View1.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("info.user.controller.View1", {
+            onInit: function () {
+                var oModel = new sap.ui.model.json.JSONModel();
+                var that = this;
+                // Make AJAX request to retrieve the current user
+                $.ajax({
+                    url: "/user-api/currentUser",
+                    method: "GET",
+                    dataType: "json",
+                    success: function (data) {
+                        // User data retrieved successfully
+                        oModel.setData(data);
+                        that.getView().byId("codeeditor").setValue(JSON.stringify(data))
+                    },
+                    error: function () {
+                        // Error occurred while retrieving user data
+                        // Handle the error accordingly
+                    }
+                });
+            }
+        });
+    });
---
+++ app/resources/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"info/user/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
